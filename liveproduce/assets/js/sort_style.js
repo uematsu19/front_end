67c6a26c00ec1ec1cc43e01dc28492d2a7510b47
hasshie53 <yuta9853@gmail.com>
2020-11-16 17:17:01 +0900

    change sort_style

--- liveproduce/assets/js/sort_style.js
+++ liveproduce/assets/js/sort_style.js
@@ -2,6 +2,7 @@
 $(document).ready(function () {
     // スマホサイズだったら
     if (window.matchMedia('(max-width:480px)').matches) {
+        // doc_form.html のみ #doc-sort が2つ
         $('#doc-sort').css('width', '200px');
         $('.sort-btn').css('font-size', '16px');
     }
